8c727dba11556e7661d686e3a4bc112bc9e940f0
Ulises Molina <uli.molina10@gmail.com>
2026-01-06 13:36:23 -0300

    add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,8 +24,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <link rel="icon" type="image/svg+xml" href="/icons8-studio-display-50 .png" />
+      <head>
+       <Script
+      id="gtm-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-57WR95TV');
+        `,
+      }}
+    />
+      </head>
       <body className={poppins.className}>{children}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57WR95TV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Analytics/>
         </body>
     </html>
